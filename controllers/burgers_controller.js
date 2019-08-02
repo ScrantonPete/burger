a32033da68f55c3eb9c0acccaa1aa43194dd0cd5
ScrantonPete <scrantonpete@yahoo.com>
2019-08-01 22:11:51 -0400

    found multiple syntax errors and inconsistencies in variable names

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,16 +12,22 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne([req.body.name], function(result) {
-    res.json({ id: result.insertId });
-  });
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devooured],
+    function(result) {
+      res.json({ id: result.insertId });
+    }
+  );
 });
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.updateOne(req.body, condition, function(result) {
-    if (result.changedRows == 0) {
+  burger.updateOne({ devoured: req.body.devoured }, condition, function(
+    result
+  ) {
+    if (result.changedRows === 0) {
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -33,7 +39,7 @@ router.delete("/api/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
+    if (result.affectedRows === 0) {
       return res.status(404).end();
     } else {
       res.status(200).end();
